e55e8cac6dcf91836452758ff68183d391b36c3a
Rachna <calgaryjains@gmail.com>
2024-04-19 17:02:04 -0600

    fixed errors related test

--- ui/login_signup/jest.config.js
+++ ui/login_signup/jest.config.js
@@ -4,4 +4,7 @@ module.exports = {
     coverageDirectory: 'coverage',
     testEnvironment: 'jsdom',
     setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    transform: {
+        "^.+\\.jsx?$": "babel-jest"
+      }
 }
--- ui/login_signup/src/App.test.js
+++ ui/login_signup/src/App.test.js
@@ -9,11 +9,11 @@ render(<Login/>);
 
     // Invalid email format
     fireEvent.change(emailInput, { target: { value: 'invalid_email' } });
-    expect(screen.getByText('Please enter a valid email address.')).toBeInTheDocument();
+    expect(screen.queryByText('Please enter a valid email address.')).toBeInTheDocument();
 
     // Valid email format
     fireEvent.change(emailInput, { target: { value: 'valid@example.com' } });
-    expect(screen.getByText('Please enter a valid email address.')).not.toBeInTheDocument();
+    expect(screen.queryByText('Please enter a valid email address.')).not.toBeInTheDocument();
   });
 
   test('validates password input', () => {
@@ -21,10 +21,10 @@ render(<Login/>);
     const passwordInput = screen.getByLabelText('Password:');
     // Invalid password (less than 6 characters)
     fireEvent.change(passwordInput, { target: { value: '12345' } });
-    expect(screen.getByText('Password must be at least 6 characters long.')).toBeInTheDocument();
+    expect(screen.queryByText('Password must be at least 6 characters long.')).toBeInTheDocument();
 
     // Valid password (at least 6 characters)
     fireEvent.change(passwordInput, { target: { value: 'password' } });
-    expect(screen.getByText('Password must be at least 6 characters long.')).not.toBeInTheDocument();
+    expect(screen.queryByText('Password must be at least 6 characters long.')).not.toBeInTheDocument();
   });
 });
